d3f06d246bd761c4025350943fc91e657735d105
Jonathan <jonathan@bangert.dk>
2022-12-17 22:34:10 +0100

    uhh

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -2,7 +2,7 @@
 	class="footer items-center p-4 bg-neutral text-neutral-content bottom-0 align-bottom sticky top-[100vh]"
 >
 	<div class="items-center grid-flow-col">
-		<svg
+		<svg aria-label="Rotated hashtag"
 			width="36"
 			height="36"
 			viewBox="0 0 24 24"
@@ -41,17 +41,8 @@
 				/></svg
 			></a
 		>
-		<a href="/blog" aria-label="Twitter"
-			><svg
-				xmlns="http://www.w3.org/2000/svg"
-				width="24"
-				height="24"
-				viewBox="0 0 24 24"
-				class="fill-current"
-				><path
-					d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"
-				/></svg
-			></a
-		>
+		<a href="/blog" aria-label="Discord">
+			<i class="bi bi-discord"></i>
+		</a>
 	</div>
 </footer>
