044b2fdb1679a844e54bfa745e67c9681c60dedd
Jonathan <jonathan@bangert.dk>
2022-12-17 11:31:33 +0100

    Add tailwind

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,27 @@
-import adapter from "@sveltejs/adapter-static"; 
+import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 // was "@sveltejs/adapter-auto"
 
-const dev = "production" === "development";
+const dev = 'production' === 'development';
 
 /** @type {import(""@sveltejs/kit").Config} */
 const config = {
-    kit: {
-        adapter: adapter({
-            pages: "docs",
-            assets: "docs"
-        }),
-        paths: {
-            base: dev ? "" : "/Un10ck3d.github.io",
-        },
-        // hydrate the <div id="svelte"> element in src/app.html
-    }
+	kit: {
+		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs'
+		}),
+		paths: {
+			base: dev ? '' : '/'
+		}
+		// hydrate the <div id="svelte"> element in src/app.html
+	},
+
+	preprocess: [
+		preprocess({
+			postcss: true
+		})
+	]
 };
 
 export default config;
-
