b5e52e3c1f3f9ca278825cab178d4fb2c271694b
Jonathan Bangert <jonathan@bangert.dk>
2023-07-24 12:33:55 +0200

    Updates

---
+++ _nuxt/config.ca5424f0.js
@@ -0,0 +1,1 @@
+import{Q as p,R as o,n as i}from"./entry.6133fc1b.js";const f={},t=p(f);function s(){const n=i();return n._appConfig||(n._appConfig=o(t)),n._appConfig}export{s as u};
--- _nuxt/config.e584b8cc.js
+++
@@ -1,1 +0,0 @@
-import{Q as p,R as o,n as i}from"./entry.c8e6671b.js";const f={},t=p(f);function s(){const n=i();return n._appConfig||(n._appConfig=o(t)),n._appConfig}export{s as u};
