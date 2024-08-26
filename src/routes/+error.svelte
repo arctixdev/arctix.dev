f60575e007880533a1f1d2ee489c86c447c7df58
Jonathan Bangert <jonathan@bangert.dk>
2024-08-25 18:53:00 -0600

    Add a bunch of metadata n stuff

---
+++ src/routes/+error.svelte
@@ -0,0 +1,28 @@
+<div>
+    <h1>
+        404 - Page not found
+    </h1>
+    <p>
+        The page you are looking for does not exist. Please check the URL and try again.
+    </p>
+</div>
+
+<style>
+    div {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 80vh;
+    }
+
+    h1 {
+        font-size: 3rem;
+        margin-bottom: 1rem;
+    }
+
+    p {
+        font-size: 1.5rem;
+        text-align: center;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,7 @@
-<div class="">
-    <h1>I am Jonathan.</h1>
-    <h2>Welcome</h2>
+<div class="w-full flex justify-center">
+    <div class="container p-20 items-center justify-center flex flex-col">
+        <img src="https://avatars.githubusercontent.com/u/74015378?v=4" alt="Jonathan Bangert" class="rounded-full shadow-lg " />
+        <h1>I am Jonathan.</h1>
+        <h2>Welcome</h2>
+    </div>
 </div>
