cf6c4164e6d50137292e67c75e8d3213ee5e10ee
Jonathan Bangert <jonathan@bangert.dk>
2025-01-22 00:08:05 -0700

    Update message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,11 @@
 <div class="w-full flex justify-center">
-    <div class="container p-20 items-center justify-center flex flex-col">
-        <img src="https://avatars.githubusercontent.com/u/74015378?v=4" alt="Jonathan Bangert" class="rounded-full shadow-lg " />
-        <h1>I am Jonathan.</h1>
-        <h2>Welcome</h2>
-    </div>
+	<div class="container p-20 items-center justify-center flex flex-col">
+		<img
+			src="https://avatars.githubusercontent.com/u/74015378?v=4"
+			alt="Jonathan Bangert"
+			class="rounded-full shadow-lg"
+		/>
+		<h1>I am Jonathan.</h1>
+		<h2>Welcome to my unfinished site..</h2>
+	</div>
 </div>
